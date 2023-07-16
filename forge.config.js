ad2ba8473b022f8ab57a61a343d0cb4c6ef7f8e7
mohitSanwal <mohit123ongraph@gmail.com>
2023-07-16 19:43:13 +0530

    publishing the app

---
+++ forge.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'mohit-sanwal',
+          name: 'basic-Electron-App'
+        },
+        authToken: process.env.GITHUB_TOKEN,
+        prerelease: false,
+        draft: true
+      }
+    }
+  ],
+  packagerConfig: {
+    asar: true,
+  },
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {},
+    },
+  ],
+  plugins: [
+    {
+      name: '@electron-forge/plugin-auto-unpack-natives',
+      config: {},
+    },
+  ],
+};
